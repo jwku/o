d3bf9f9f6788af2d55a5c1cb9e5a669aea72863c
jwku <116755047+jwku@users.noreply.github.com>
2025-09-07 23:30:19 -0600

    [If omitted, spaces and tabs will be removed] NewString :=  Trim(String [, OmitChars]) https://www.autohotkey.com/docs/v2/lib/Trim.htm
    NewString :=  Trim(String [, OmitChars])
    https://www.autohotkey.com/docs/v2/lib/Trim.htm 
    
    If omitted, spaces and tabs will be removed. Otherwise, specify a list of characters (case-sensitive) to exclude from the beginning and/or end of String.

---
+++ 5 0907 2329Trim&LTrim&RTrim.rs
@@ -0,0 +1,5 @@
+NewString :=  Trim(String [, OmitChars])
+https://www.autohotkey.com/docs/v2/lib/Trim.htm 
+
+If omitted, spaces and tabs will be removed. Otherwise, specify a list of characters (case-sensitive) to exclude from the beginning and/or end of String.
+
