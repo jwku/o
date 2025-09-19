185538d8c2a898fae5dd3995575a461235ab5049
jwku <116755047+jwku@users.noreply.github.com>
2025-09-19 13:38:49 -0600

    .self, in Python, just a convention to mean .this   OTHERWISE in langs, talking about “the class as a whole”  .this when talking about “me, this specific object”   https://chatgpt.com/c/68cdb071-bae8-832c-ab1a-197a8fefd279
    .self, in Python, just a convention to mean .this 
    
    OTHERWISE in langs, talking about “the class as a whole”
    
    .this when talking about “me, this specific object” 
    
    https://chatgpt.com/c/68cdb071-bae8-832c-ab1a-197a8fefd279

---
+++ 5 0919 1338.ps1
@@ -0,0 +1,7 @@
+.self, in Python, just a convention to mean .this 
+
+OTHERWISE in langs, talking about “the class as a whole”
+
+.this when talking about “me, this specific object” 
+
+https://chatgpt.com/c/68cdb071-bae8-832c-ab1a-197a8fefd279
